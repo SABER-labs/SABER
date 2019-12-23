891916559a4dd8c2ae71b75d2c371b54c2428665
Arjun Variar <Arjun.Variar@airtel.com>
2019-12-23 08:24:03 +0530

    adding training script

--- train.sh
+++ train.sh
@@ -1,5 +1,3 @@
-sh sync.sh
-# rm -rf checkpoints_logs/exp-sp-nonfocal-vocab128-new checkpoints/
-# rm -rf checkpoints_logs/exp-sp-ctc-vocab128-radam checkpoints_5x3_radam/
-rm -rf checkpoints_logs/exp-sp-ctc-vocab128-radam/
+# sh sync.sh
+# rm -rf checkpoints_logs/exp-checkpoints_saber_128 checkpoints_saber_128
 CUDA_VISIBLE_DEVICES="0,1,2" python3.6 train.py
