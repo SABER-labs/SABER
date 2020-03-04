b7bf4ca7f31a8f34ea77e324766ce870a52275b3
Arjun Variar <Arjun.Variar@airtel.com>
2020-03-04 08:08:51 +0530

    character level

--- utils/vocab.py
+++ utils/vocab.py
@@ -5,6 +5,7 @@ class Vocab(object):
     def __init__(self, blank_id=0, vocab=string.ascii_lowercase + string.whitespace[0]):
         super().__init__()
         self.vocab = vocab
+        self.blank_id = blank_id
 
     def encode(self, text):
         idxs = [self.vocab.index(c) + 1 for c in text.lower() if c in self.vocab]
@@ -16,7 +17,7 @@ class Vocab(object):
         return idxs
 
     def decode(self, tokens):
-        return ''.join([self.vocab[token-1] for token in tokens if token > 0])
+        return ''.join([self.vocab[token-1] for token in tokens if token != self.blank_id])
 
     def __len__(self):
         return len(self.vocab) + 1
