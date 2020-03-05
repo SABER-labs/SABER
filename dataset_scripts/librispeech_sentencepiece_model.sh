edc5704fb62a1d27b0260034e0f9c1d3404d15be
Arjun Variar <Arjun.Variar@airtel.com>
2020-03-05 08:46:26 +0530

    char sentencepiece

--- dataset_scripts/librispeech_sentencepiece_model.sh
+++ dataset_scripts/librispeech_sentencepiece_model.sh
@@ -1,2 +1,2 @@
 spm_train --input=/tts_data/saber/dataset_scripts/single_text.txt --model_prefix=sp_librispeech_64 --vocab_size=64 --character_coverage=1.0 --model_type=bpe --bos_id=-1 --eos_id=2 --pad_id=0 --unk_id=1 --max_sentencepiece_length=500 --num_threads=32
-# spm_train --input=/tts_data/asrdata/download_scripts/wiki.en.txt --model_prefix=wiki --max_sentence_length=500 --vocab_size=1024 --model_type=bpe --bos_id=-1 --eos_id=2 --pad_id=0 --unk_id=1 --max_sentencepiece_length=188 --num_threads=32 --character_coverage=0.995 --input_sentence_size=1000000
+# spm_train --input=/tts_data/saber/dataset_scripts/single_text.txt --model_prefix=sp_librispeech_29 --model_type=char --character_coverage=1.0 --bos_id=-1 --eos_id=-1 --unk_id=0 --num_threads=32
