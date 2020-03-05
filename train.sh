6d1137a6db88d50b07c86408e0e78f48e3222ac9
Arjun Variar <Arjun.Variar@airtel.com>
2020-03-05 06:43:45 +0530

    changes

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 sh sync.sh
-rm -rf checkpoints_logs/exp-checkpoints_saber_28 checkpoints_saber_28
+rm -rf checkpoints_logs/exp-checkpoints_saber_128 checkpoints_saber_128
 OMP_NUM_THREADS="1" CUDA_VISIBLE_DEVICES="0,1,2" python3.6 train.py
