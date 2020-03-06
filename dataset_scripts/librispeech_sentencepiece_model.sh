38c582737e49f3ce56b7f8d8e58cb5af75799c85
Arjun Variar <Arjun.Variar@airtel.com>
2020-03-06 10:15:38 +0530

    try smaller segments

--- dataset_scripts/librispeech_sentencepiece_model.sh
+++ dataset_scripts/librispeech_sentencepiece_model.sh
@@ -1,2 +1,2 @@
-spm_train --input=/tts_data/saber/dataset_scripts/single_text.txt --model_prefix=sp_librispeech_64 --vocab_size=64 --character_coverage=1.0 --model_type=bpe --bos_id=-1 --eos_id=2 --pad_id=0 --unk_id=1 --max_sentencepiece_length=500 --num_threads=32
+spm_train --input=/tts_data/saber/dataset_scripts/single_text.txt --model_prefix=sp_librispeech_1024 --max_sentencepiece_length=3 --vocab_size=1024 --character_coverage=1.0 --model_type=bpe --bos_id=-1 --eos_id=-1 --unk_id=0 --num_threads=32
 # spm_train --input=/tts_data/saber/dataset_scripts/single_text.txt --model_prefix=sp_librispeech_29 --model_type=char --character_coverage=1.0 --bos_id=-1 --eos_id=-1 --unk_id=0 --num_threads=32
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 sh sync.sh
-rm -rf checkpoints_logs/exp-checkpoints_saber_29 checkpoints_saber_29
+rm -rf checkpoints_logs/exp-checkpoints_saber_1024 checkpoints_saber_1024
 OMP_NUM_THREADS="1" CUDA_VISIBLE_DEVICES="0,1,2" python3.6 train.py
