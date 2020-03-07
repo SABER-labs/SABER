c8ceb749055a751df02fa2e50b596f52c4983663
Arjun Variar <Arjun.Variar@airtel.com>
2020-03-07 16:45:13 +0530

    Don't delete the model, since it works well

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 sh sync.sh
-rm -rf checkpoints_logs/exp-checkpoints_saber_1024 checkpoints_saber_1024
+# rm -rf checkpoints_logs/exp-checkpoints_saber_1024 checkpoints_saber_1024
+rm -rf checkpoints_logs/exp-checkpoints_saber_1024
 OMP_NUM_THREADS="1" CUDA_VISIBLE_DEVICES="0,1,2" python3.6 train.py
